adbe2462d7c9f74b898cfb34282e68247620cfbb
Dibyn <dibyn7@gmail.com>
2021-04-29 18:33:04 +0545

    separated header as component for login page

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MainHead from 'components/head'
 import { Form, Input, Button, Checkbox } from 'antd'
 const layout = {
 	labelCol: {
@@ -23,48 +24,51 @@ const Login = () => {
 		console.log('Failed:', errorInfo)
 	}
 	return (
-		<Form
-			{...layout}
-			name="basic"
-			initialValues={{
-				remember: true
-			}}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-		>
-			<Form.Item
-				label="Username"
-				name="username"
-				rules={[
-					{
-						required: true,
-						message: 'Please input your username!'
-					}
-				]}
+		<>
+			<MainHead title={'Login'} />
+			<Form
+				{...layout}
+				name="basic"
+				initialValues={{
+					remember: true
+				}}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
 			>
-				<Input />
-			</Form.Item>
-			<Form.Item
-				label="Password"
-				name="password"
-				rules={[
-					{
-						required: true,
-						message: 'Please input your password!'
-					}
-				]}
-			>
-				<Input.Password />
-			</Form.Item>
-			<Form.Item {...tailLayout} name="remember" valuePropName="checked">
-				<Checkbox>Remember me</Checkbox>
-			</Form.Item>
-			<Form.Item {...tailLayout}>
-				<Button type="primary" htmlType="submit">
-					Submit
-				</Button>
-			</Form.Item>
-		</Form>
+				<Form.Item
+					label="Username"
+					name="username"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your username!'
+						}
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					label="Password"
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: 'Please input your password!'
+						}
+					]}
+				>
+					<Input.Password />
+				</Form.Item>
+				<Form.Item {...tailLayout} name="remember" valuePropName="checked">
+					<Checkbox>Remember me</Checkbox>
+				</Form.Item>
+				<Form.Item {...tailLayout}>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
 	)
 }
 export default Login

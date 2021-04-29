478f451b7b4530a93830be3b2b06d394bd702ab9
Raj Shrestha <raj@uxcam.com>
2021-04-29 15:47:35 +0545

    added html for chart

--- components/Dashboard/BugChart.js
+++ components/Dashboard/BugChart.js
@@ -6,27 +6,35 @@ const BugChart = () => (
 		highcharts={Highcharts}
 		options={{
 			title: {
-				text: 'Monthly Average Temperature',
-				x: -20 //center
-			},
-			subtitle: {
-				text: 'Source: WorldClimate.com',
-				x: -20
+				text: 'Bug Occurences'
 			},
 			xAxis: {
 				categories: [
-					'Jan',
-					'Feb',
-					'Mar',
-					'Apr',
-					'May',
-					'Jun',
-					'Jul',
-					'Aug',
-					'Sep',
-					'Oct',
-					'Nov',
-					'Dec'
+					'05:00',
+					'06:00',
+					'07:00',
+					'08:00',
+					'09:00',
+					'10:00',
+					'11:00',
+					'12:00',
+					'13:00',
+					'14:00',
+					'15:00',
+					'16:00',
+					'17:00',
+					'18:00',
+					'19:00',
+					'20:00',
+					'21:00',
+					'22:00',
+					'23:00',
+					'00:00',
+					'01:00',
+					'02:00',
+					'03:00',
+					'04:00',
+					'05:00'
 				]
 			},
 			yAxis: {
@@ -37,35 +45,41 @@ const BugChart = () => (
 					{
 						value: 0,
 						width: 1,
-						color: '#808080'
+						color: '#EB5757'
 					}
 				]
 			},
 			tooltip: {
 				valueSuffix: '°C'
 			},
-			legend: {
-				layout: 'vertical',
-				align: 'right',
-				verticalAlign: 'middle',
-				borderWidth: 0
-			},
 			series: [
 				{
-					name: 'Tokyo',
+					name: '',
 					data: [
-						7.0,
-						6.9,
-						9.5,
-						14.5,
-						18.2,
-						21.5,
-						25.2,
-						26.5,
-						23.3,
-						18.3,
-						13.9,
-						9.6
+						0,
+						1,
+						0,
+						2,
+						0,
+						0,
+						5,
+						1,
+						0,
+						0,
+						3,
+						0,
+						0,
+						0,
+						1,
+						0,
+						0,
+						0,
+						3,
+						0,
+						0,
+						0,
+						0,
+						1
 					]
 				}
 			]

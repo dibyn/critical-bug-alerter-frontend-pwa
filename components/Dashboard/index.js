302d18c32e08ea6968d9d8f138b7058552090e58
Dibyn <dibyn7@gmail.com>
2021-04-29 14:45:01 +0545

    added chart for bug logs

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,8 +1,12 @@
 import TableOfBugs from 'components/Dashboard/TableOfBugs'
+import BugChart from './BugChart'
 const index = () => {
 	return (
 		<div className="body-wrapper">
-			<div className="chart-box">Chart</div>
+			<div className="chart-box">
+				Chart
+				<BugChart />
+			</div>
 			<div className="table-box">
 				Table
 				<TableOfBugs />

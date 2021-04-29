49eaaa57c466361096b398d1491aad25c64ca14e
Dibyn <dibyn7@gmail.com>
2021-04-29 14:24:12 +0545

    added table

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,8 +1,12 @@
+import TableOfBugs from 'components/Dashboard/TableOfBugs'
 const index = () => {
 	return (
 		<div className="body-wrapper">
 			<div className="chart-box">Chart</div>
-			<div className="table-box">Table</div>
+			<div className="table-box">
+				Table
+				<TableOfBugs />
+			</div>
 		</div>
 	)
 }
